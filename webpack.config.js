dcaa10c989a10ce2bf6205ded155b755471a0e64
lester fernandes <lestr.169@gmail.com>
2022-01-09 10:25:01 +0000

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
-module.exports = {
-    mode: "development",
-    devtool: false,
-    module:{
-        rules:[{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        }]
-    },
-    devServer: {
-        static: "./ dist"
-    }
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
+
+module.exports = {
+  mode: mode,
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+  },
+};
