295f99fdb7269d1c02171906253622ba77a3aafc
lester <lestr.169@gmail.com>
2022-01-10 15:02:55 +0530

    refactoring, added react refresh for dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HTMLWebpackPlugin({
+    template: "./src/index.html",
+  }),
+];
 let mode = "development";
 if (process.env.NODE_ENV === "production") {
   mode = "production";
+} else {
+  plugins.push(new ReactRefreshPlugin());
 }
 
 module.exports = {
   mode: mode,
   devtool: "source-map",
+  entry: "./src/index.js",
   module: {
     rules: [
       {
@@ -26,21 +37,15 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HTMLWebpackPlugin({
-      template: "./src/index.html"
-    }),
-  ],
+  plugins: plugins,
   devServer: {
     static: "./dist",
-    hot: true
+    hot: true,
   },
   resolve: {
-      extensions: [".js", ".jsx"]
+    extensions: [".js", ".jsx"],
   },
   output: {
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+  },
 };
